8e8c494926edfa1a116bb84dabbd844d1483750b
Tony Messias <tonyzrp@gmail.com>
2019-09-09 16:20:17 -0300

    move the state and client handler instances to outside the loop

--- bin/phredis.php
+++ bin/phredis.php
@@ -9,14 +9,15 @@ use Tonysm\Phredis\State;
 use function Amp\asyncCall;
 use Tonysm\Phredis\Server as PhredisServer;
 
-Loop::run(function () use ($port) {
+$state = new State();
+$clientHandler = new PhredisServer($state);
+
+Loop::run(function () use ($port, $clientHandler) {
     $uri = "tcp://127.0.0.1:{$port}";
 
     echo "Stared running Phredis Server at {$uri}\n";
 
-    $state = new State();
     $server = Amp\Socket\Server::listen($uri);
-    $clientHandler = new PhredisServer($state);
 
     while ($socket = yield $server->accept()) {
         asyncCall($clientHandler, $socket);
